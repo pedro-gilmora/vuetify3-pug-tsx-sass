f3b83cd9b10df8d2b5e03a289a0cdbcb4bc391b2
Pedro Gil Mora <MY_NAME@example.com>
2022-04-25 23:48:29 -0400

    remove comments

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,6 @@ v-app
 </template>
 
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from '@/components/HelloWorld.vue';
 import Welcome from '@/components/Welcome';
 import {VApp} from 'vuetify/components';
